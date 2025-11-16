f0f1b027d651f9e3ecd117906d84eb2a519fa450
Pseudo Mortus <104158227+PseudoMortus@users.noreply.github.com>
2025-11-16 13:53:12 -0600

    Porting the rest over as much as possible, finding the button to not work as intended on Home page

--- PhilanthroPoints/obj/Debug/net8.0/PhilanthroPoints.AssemblyInfo.cs
+++ PhilanthroPoints/obj/Debug/net8.0/PhilanthroPoints.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PhilanthroPoints")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+018060ed1f507e35aed7852b66e82519811d3aa8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdef405eb295989c47a38248d900ffc85157e6e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("PhilanthroPoints")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PhilanthroPoints")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
