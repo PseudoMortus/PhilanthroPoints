aadee1dceb4083c42d35b2ac38d02aebecbfede2
Pseudo Mortus <104158227+PseudoMortus@users.noreply.github.com>
2025-11-21 09:00:02 -0600

    Converting to Laptop

--- obj/Debug/net8.0/PhilanthroPoints.AssemblyInfo.cs
+++ obj/Debug/net8.0/PhilanthroPoints.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PhilanthroPoints")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c692ef35a313aa13c87e6eae17092468a90c8fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c7cb29afdc232892ba56583182be87ceafa7a09")]
 [assembly: System.Reflection.AssemblyProductAttribute("PhilanthroPoints")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PhilanthroPoints")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
