ffd981e75975ca04105c38aaa496bb68bc6172e6
mcriswell86 <mcriswell@icstars.org>
2025-11-24 15:31:43 -0600

    Creating a blank slate to merge from Main

--- Models/Category.cs
+++
@@ -1,1 +0,0 @@
-namespace PhilanthroPoints.Models; public class Category { public int Id {get;set;} public string Name {get;set;} = string.Empty; }
--- wwwroot/points.js
+++
@@ -1,9 +0,0 @@
-
-window.pointsStore = {
-  load: function(){
-    const raw = localStorage.getItem('points');
-    return raw ? JSON.parse(raw) : { value: 0, name: null };
-  },
-  save: function(obj){ localStorage.setItem('points', JSON.stringify(obj)); },
-  clear: function(){ localStorage.removeItem('points'); }
-};
