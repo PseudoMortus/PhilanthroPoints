16c751330396b19dd1a7966d78582c4987e7b643
mcriswell86 <mcriswell@icstars.org>
2025-11-27 10:14:29 -0600

    new updates

--- obj/Debug/net8.0/PhilanthroPoints.AssemblyInfo.cs
+++ obj/Debug/net8.0/PhilanthroPoints.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PhilanthroPoints")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2a2f4753c3f63ede01e494bec6e7ffa54521b78")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11af7c7c166de00f258367f46ca5fed0a2628ad6")]
 [assembly: System.Reflection.AssemblyProductAttribute("PhilanthroPoints")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PhilanthroPoints")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
