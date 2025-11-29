8fa344d3eecb8d15f4f9ee97545cf411acf1f32e
PseudoMortus <johnathanc2359@pseudoservices.com>
2025-11-28 21:35:14 -0600

    Summary: Adding Fonts and Logo (With Variants), Updating Home Page to Use Fonts, and Implementing Logos Incorporated Logos and Fonts into the following pages 	modified:   Components/Layout/MainLayout.razor 	modified:   Pages/Home.razor
    Created Font Families for the following fonts:
    	modified:   wwwroot/app.css
    	new file:   wwwroot/fonts/Feeling-Passionate.ttf
    	new file:   wwwroot/fonts/Gotham.otf
    	new file:   wwwroot/fonts/LilitaOne-Regular.ttf
    
    Added logos and necessary files to modify logosL
    	new file:   wwwroot/images/Monochrome-BDay-Connections-Logo-Black.png
    	new file:   wwwroot/images/Monochrome-BDay-Connections-Logo-Black.png-autosave.kra
    	new file:   wwwroot/images/Monochrome-BDay-Connections-Logo-Black.png~
    	new file:   wwwroot/images/Monochrome-BDay-Connections-Logo-White.png

--- obj/Debug/net8.0/PhilanthroPoints.AssemblyInfo.cs
+++ obj/Debug/net8.0/PhilanthroPoints.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PhilanthroPoints")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f9a14acae988087e761008cb3b87d486b43509d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de8897d8d5ee86b75fb657fd2ea61d5d227aed47")]
 [assembly: System.Reflection.AssemblyProductAttribute("PhilanthroPoints")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PhilanthroPoints")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
