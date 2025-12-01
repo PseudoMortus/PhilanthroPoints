f0cc04ae7dd65e0736a614fbb38b83b21184a965
PseudoMortus <johnathanc2359@pseudoservices.com>
2025-12-01 13:06:23 -0600

    Cleaned up some of the code, added more images

--- obj/Debug/net8.0/PhilanthroPoints.AssemblyInfo.cs
+++ obj/Debug/net8.0/PhilanthroPoints.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PhilanthroPoints")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9067e71cd4587fc26bb4a722d5c198517897750")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f30faf49cae15f8c0acc2a2c93fdd7ee81a5d62")]
 [assembly: System.Reflection.AssemblyProductAttribute("PhilanthroPoints")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PhilanthroPoints")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
