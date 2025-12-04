d850f631328acc63d6a55465941f95b04727ba27
PseudoMortus <johnathanc2359@pseudoservices.com>
2025-12-03 19:14:55 -0600

    converting to Pseudo's Laptop

--- obj/Debug/net8.0/PhilanthroPoints.AssemblyInfo.cs
+++ obj/Debug/net8.0/PhilanthroPoints.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PhilanthroPoints")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fb2a002b92ba08c0b79708de6e626e1ad15b1aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d006a00d5c9d004e3ecbe513aa191ac6b93520aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("PhilanthroPoints")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PhilanthroPoints")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
